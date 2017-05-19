8dc9f31acc6d49e7d50d9a6c79be1681fa5b4cf1
YuyuZha0 <YuyuZha0@github.com>
2017-05-19 16:01:48 +0800

    first commit

---
+++ src/main/java/com/acc/yys/controller/ApiController.java
@@ -0,0 +1,10 @@
+package com.acc.yys.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhaoyy on 2017/5/19.
+ */
+@RestController
+public final class ApiController {
+}
---
+++ src/main/java/com/acc/yys/controller/IndexController.java
@@ -0,0 +1,10 @@
+package com.acc.yys.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by zhaoyy on 2017/5/19.
+ */
+@Controller
+public final class IndexController {
+}
